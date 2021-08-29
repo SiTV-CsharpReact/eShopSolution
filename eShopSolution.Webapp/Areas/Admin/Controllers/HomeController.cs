18f5ee545c58c6df979e28640ed099ae537c53c5
văn sĩ <vansi19a@gmail.com>
2021-08-29 16:58:37 +0700

    create solution structure

---
+++ eShopSolution.Webapp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace eShopSolution.Webapp.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
