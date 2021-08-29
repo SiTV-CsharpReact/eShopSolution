39c144e8905175960bef6bfe70e24cfb0cfd7c5d
văn sĩ <vansi19a@gmail.com>
2021-08-29 17:38:48 +0700

    day danh sach

---
+++ eShopSolution.Data/EF/EshopDBContetxt.cs
@@ -0,0 +1,17 @@
+﻿using eShopSolution.Data.Entiles;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EshopDBContetxt : DbContext
+    {
+        public EshopDBContetxt(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Categories { get; set; }
+    }
+}
